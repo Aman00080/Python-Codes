a588b0885d9cfd39bc8c08c201d5360fc9ef82b9
Aman  Prasad <mishraamanprasad@gmail.com>
2025-06-16 14:36:52 +0530

    Add files via upload
    basic of python ,more to come in future ...

---
+++ AMAN.py
@@ -0,0 +1,73 @@
+# str="my name is aman "
+# ch=str[0]
+# print(ch)
+# str="hello , my name is aman "
+# print(str[4])
+# str="hello , how are you"
+# print(str[:4])
+# print(str[0:6])
+# print(str[-1:-3])
+# a=int(input("enter you mark :"))
+# b=str(input("enter your grade "))
+# print(type(a))
+# print(type(b))
+# if(a>=90):
+#     print("pass")
+# elif(a!=90):
+#     print("fail")
+# str="my name is aman"
+# print(str.endswith("an"))
+# print(str.replace("aman","rahul"))
+# print(str.capitalize())
+# str=(str.capitalize())
+# print(str)
+# print(str.find("name"))
+# print(str.count("a"))
+# str=input("enter your name :")
+# print('length of name is:',len(str))
+
+# str="helllo , hows are yous i hope every things will be okks"
+# print(str.count("s"))
+# marks=[87,88,55,45,86,99]
+# print(marks[0])
+# print(marks[4])
+# student=["aman","raju","rohit","rahul"]
+# print(student[0])
+# print(student[3])
+# len=len(student)
+# print(len)
+# list=[1,2,3,4,5,6]
+# list.append(8)
+# print(list)
+# print(list[4])
+# list[4]=7
+# print(list)
+# list=["aman","rohit"]
+# list.append("raj")
+# print(list)
+# fruit_stole=['apple','grapes','onion','mango']
+# fruit_stole.sort(reverse=True)
+# print(fruit_stole)
+# student=[1,3,4,5,6,7,78,]
+# student.sort(reverse=True)
+# print(student)
+# fruit_stole.reverse()
+# print(fruit_stole)
+# student.reverse()
+# print(student)
+# student=[1,1,1,13,3,3,4,4,5,5]
+# # student.remove(1)
+# # student.remove(4)
+# # print(student)
+# student.insert(1,2)
+# print(student)
+# student.pop(1)
+# print(student)
+
+
+#ex:-1
+a=str(input("enter your name :"))
+if(a==str):
+    print(a,"this is string char:")
+else:
+    print("given name is not a string:")
